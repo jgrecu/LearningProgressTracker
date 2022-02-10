4445a4d08e1032b9604dca6433506ae3b61228c0
Jeremy Grecu <jeremy.grecu@sap.com>
2022-02-10 10:44:06 +0000

    JBA - Learning Progress Tracker - Stage 1/5: No empty lines here

--- task/src/tracker/Main.java
+++ task/src/tracker/Main.java
@@ -1,7 +1,22 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final Scanner scanner = new Scanner(System.in);
+        String input = "";
+        System.out.println("Learning Progress Tracker.");
+        while (true) {
+            input = scanner.nextLine().toLowerCase().strip();
+            if (input.isEmpty() || input.isBlank()) {
+                System.out.println("No input.");
+            } else if ("exit".equals(input)) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println("Error: unknown command!");
+            }
+        }
     }
 }
